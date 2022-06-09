9fd35ee5809c9a91e9f9e215d87f1f7b53e1b7d2
MosiurRrahman <mosiurrahman595@gmail.com>
2022-06-09 17:40:50 +0600

    add modal

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -6,8 +6,40 @@ function Main() {
       <div className="main">
         <div className="container">
           <div className="row">
+
             <div className="col-lg-3">
               <div className="clock-wrap">
+              <button type="button" className="btn clock-btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">add clock</button>
+        <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">New message</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+              </div>
+              <div className="modal-body">
+                <form>
+                  <div className="mb-3">
+                    <label htmlFor="recipient-name" className="col-form-label">Recipient:</label>
+                    <input type="text" className="form-control" id="recipient-name" />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="message-text" className="col-form-label">Message:</label>
+                    <textarea className="form-control" id="message-text" defaultValue={""} />
+                  </div>
+                </form>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Send message</button>
+              </div>
+            </div>
+          </div>
+        </div>
+                
+                
+               
+               
                 <div className="clock-list">
                   <div className="clock-content">
                     <h6>clock title</h6>
@@ -17,6 +49,8 @@ function Main() {
                     <h6>time zone:</h6>
                     <h6>current time</h6>
                   </div>
+                  <button>delete clock</button>
+                  <button>add event</button>
                 </div>
                 <div className="clock-list">
                   <div className="clock-content">
@@ -27,6 +61,32 @@ function Main() {
                     <h6>time zone:</h6>
                     <h6>current time</h6>
                   </div>
+                  <button>delete clock</button>
+                  <button>add event</button>
+                </div>
+                <div className="clock-list">
+                  <div className="clock-content">
+                    <h6>clock title</h6>
+                    <h6>clock description</h6>
+                  </div>
+                  <div className="clock-info">
+                    <h6>time zone:</h6>
+                    <h6>current time</h6>
+                  </div>
+                  <button>delete clock</button>
+                  <button>add event</button>
+                </div>
+                <div className="clock-list">
+                  <div className="clock-content">
+                    <h6>clock title</h6>
+                    <h6>clock description</h6>
+                  </div>
+                  <div className="clock-info">
+                    <h6>time zone:</h6>
+                    <h6>current time</h6>
+                  </div>
+                  <button>delete clock</button>
+                  <button>add event</button>
                 </div>
               </div>
             </div>
@@ -79,6 +139,7 @@ function Main() {
                       <button>edit</button>
                       <button>delete</button>
                     </div>
+                   
                   </div>
                 </div>
                 <div className="col-lg-6">
